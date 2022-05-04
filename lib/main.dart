25668fe323338d2abee5bea49381cd2abac8c11a
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-05-03 23:29:03 -0600

    fixed bugs, cannot have home specified if initial route is also specified

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Screen1(),
       initialRoute: "/",
       routes: {
         '/': (context) => Screen1(),
